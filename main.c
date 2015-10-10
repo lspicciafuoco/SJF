72a45999d34a01ba089228b232278c9b819cc489
Alessio Picciafuoco <alessio.picciafuoco@gmail.com>
2015-10-11 00:11:57 +0200

    Completed

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Alessio
+ *
+ * Created on 11 ottobre 2015, 0.07
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
